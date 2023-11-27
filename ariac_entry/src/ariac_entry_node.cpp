4d48ab294e1ea4a83e8cc6a7ae22c40e32b89269
Georgia Martinez <gcm49@case.edu>
2023-11-27 16:56:04 +0000

    Started trajectory topic stuff

--- ariac_entry/src/ariac_entry_node.cpp
+++ ariac_entry/src/ariac_entry_node.cpp
@@ -15,7 +15,9 @@
 #include "geometry_msgs/TransformStamped.h"
 #include "geometry_msgs/Pose.h"
 
+#include "ur_kinematics/ur_kin.h"
 #include "sensor_msgs/JointState.h"
+#include "trajectory_msgs/JointTrajectory.h"
 
 std::vector<osrf_gear::Order> orders;
 osrf_gear::LogicalCameraImage::ConstPtr camera_images[10];
@@ -36,6 +38,10 @@ std::string camera_topics[] = {
 	"/ariac/quality_control_sensor_1",
 	"/ariac/quality_control_sensor_2"
 };
+
+// Instantiate variables for use with the kinematic system.
+double T_pose[4][4], T_des[4][4];
+double q_pose[6], q_des[8][6];
 
 bool startCompetition(ros::NodeHandle &n) {
 	ROS_INFO("Waiting for the competition to start...");
@@ -105,6 +111,118 @@ void moveArm(osrf_gear::Model model, std::string sourceFrame) {
 	
 	geometry_msgs::Point position = goal_pose.pose.position;
 	ROS_WARN("Position: x=%f, y=%f, z=%f (relative to arm)", position.x, position.y, position.z);
+}
+
+trajectory_msgs::JointTrajectory setupJointTrajectory() {
+	
+	trajectory_msgs::JointTrajectory joint_trajectory;
+    
+    int count = 0;
+    joint_trajectory.header.seq = count++;
+    joint_trajectory.header.stamp = ros::Time::now(); // When was this message created.
+    joint_trajectory.header.frame_id = "/world"; // Frame in which this is specified
+
+    // Set the names of the joints being used. All must be present.
+    joint_trajectory.joint_names.clear();
+
+    joint_trajectory.joint_names.push_back("linear_arm_actuator_joint");
+    joint_trajectory.joint_names.push_back("shoulder_pan_joint");
+    joint_trajectory.joint_names.push_back("shoulder_lift_joint");
+    joint_trajectory.joint_names.push_back("elbow_joint");
+    joint_trajectory.joint_names.push_back("wrist_1_joint");
+    joint_trajectory.joint_names.push_back("wrist_2_joint");
+    joint_trajectory.joint_names.push_back("wrist_3_joint");
+
+    // Set a start and end point.
+    joint_trajectory.points.resize(2);
+
+    //  Set the start point to the current position of the joints from joint_states.
+    joint_trajectory.points[0].positions.resize(joint_trajectory.joint_names.size());
+
+    // When to start (immediately upon receipt).
+    joint_trajectory.points[0].time_from_start = ros::Duration(0.01);
+
+    joint_trajectory.points[0].positions.resize(joint_trajectory.joint_names.size());
+    joint_trajectory.points[1].positions.resize(joint_trajectory.joint_names.size());
+    
+    return joint_trajectory;	
+}
+
+trajectory_msgs::JointTrajectory get_trajectory(geometry_msgs::Point dest) {
+	// Where is the end effector given the joint angles.
+	// joint_states.position[0] is the linear_arm_actuator_joint
+	q_pose[0] = joint_states.position[1];
+	q_pose[1] = joint_states.position[2];
+	q_pose[2] = joint_states.position[3];
+	q_pose[3] = joint_states.position[4];
+	q_pose[4] = joint_states.position[5];
+	q_pose[5] = joint_states.position[6];
+	
+	ur_kinematics::forward((double*) &q_pose, (double*) &T_pose);
+
+	// What joint angles put the end effector at a specific place.
+	// Desired pose of the end effector wrt the base_link.
+	T_des[0][3] = (double) dest.x;
+	T_des[1][3] = (double) dest.y;
+	T_des[2][3] = (double) dest.z;
+	T_des[3][3] = 1.0;
+	
+	// The orientation of the end effector so that the end effector is down.
+	T_des[0][0] = 0.0; T_des[0][1] = -1.0; T_des[0][2] = 0.0;
+	T_des[1][0] = 0.0; T_des[1][1] = 0.0; T_des[1][2] = 1.0;
+	T_des[2][0] = -1.0; T_des[2][1] = 0.0; T_des[2][2] = 0.0;
+	T_des[3][0] = 0.0; T_des[3][1] = 0.0; T_des[3][2] = 0.0;
+	
+	int num_sols = ur_kinematics::inverse((double*) &T_des, (double*) &q_des);
+
+	trajectory_msgs::JointTrajectory joint_trajectory = setupJointTrajectory();
+	
+	if (num_sols == 0) {
+		ROS_WARN("NO INITIAL SOLUTIONS FOUND");
+		joint_trajectory.header.frame_id = "empty";
+        return joint_trajectory;
+	}
+
+	
+	// Set the start point to the current position of the joints from joint_states.
+	joint_trajectory.points[0].positions.resize(joint_trajectory.joint_names.size());
+	
+	for (int indy = 0; indy < joint_trajectory.joint_names.size(); indy++) {
+		for (int indz = 0; indz < joint_states.name.size(); indz++) {
+			if (joint_trajectory.joint_names[indy] == joint_states.name[indz]) {
+				joint_trajectory.points[0].positions[indy] = joint_states.position[indz];
+				break;
+			}
+		}
+	}
+		
+	// Find a good solution
+	int solution_index = -1;
+	
+	for (int i = 0; i < num_sols; i++) {
+		double shoulder_angle = q_des[i][1];
+		double wrist_1_angle = q_des[i][3];
+
+		if (abs(M_PI - wrist_1_angle) <= M_PI / 2 && shoulder_angle >= 4 * M_PI / 3) {
+			solution_index = i;
+			break;
+		}
+	}
+	
+	if (solution_index == -1) {
+		ROS_WARN("NO VIABLE SOLUTIONS FOUND");
+		joint_trajectory.header.frame_id = "empty";
+        return joint_trajectory;
+	}
+	
+	// Enter the joint positions in the correct positions
+	for (int indy = 0; indy < 6; indy++) {
+        joint_trajectory.points[1].positions[indy + 1] = q_des[solution_index][indy];
+    }
+
+    joint_trajectory.points[1].time_from_start = ros::Duration(1.0);
+	
+	return joint_trajectory;
 }
 
 void processOrder() {
@@ -184,7 +302,7 @@ int main(int argc, char **argv) {
 	locationClient = n.serviceClient<osrf_gear::GetMaterialLocations>("/ariac/material_locations");
 	
 	// Subscribe to orders topic
-	ros::Subscriber sub = n.subscribe<osrf_gear::Order>("/ariac/orders", 1000, orderCallback);
+	ros::Subscriber order_sub = n.subscribe<osrf_gear::Order>("/ariac/orders", 1000, orderCallback);
 
 	// Subscribe to camera topics	
 	std::vector<ros::Subscriber> camera_subs;
@@ -194,7 +312,7 @@ int main(int argc, char **argv) {
 	}
 	
 	// Subscribe to joints topic
-	ros::Subscriber jointSub = n.subscribe<sensor_msgs::JointState>("/ariac/arm1/joint_states", 1000, jointCallback);
+	ros::Subscriber joint_sub = n.subscribe<sensor_msgs::JointState>("/ariac/arm1/joint_states", 1000, jointCallback);
 
 	// Start the competition
 
